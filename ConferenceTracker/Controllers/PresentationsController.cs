4bb5196e4ed34844ba0c5a132963a1121b58043e
Shivam Guness <gshivam2000@yahoo.com>
2020-12-02 17:03:27 +0400

    Added logWarning

--- ConferenceTracker/Controllers/PresentationsController.cs
+++ ConferenceTracker/Controllers/PresentationsController.cs
@@ -69,14 +69,14 @@ namespace ConferenceTracker.Controllers
             _logger.LogInformation("Getting presentation id:" + id + " for edit.");
             if (id == null)
             {
-                _logger.LogError("Presentation id was null.");
+                _logger.LogError("Presentation id is null");
                 return NotFound();
             }
 
             var presentation = _presentationRepository.GetPresentation((int)id);
             if (presentation == null)
             {
-                _logger.LogError("Presentation id, " + id + ", was not found.");
+                _logger.LogWarning("Presentation id, " + id + ", was not found.");
                 return NotFound();
             }
             _logger.LogInformation("Presentation id," + id + ", was found. Returning 'Edit view'");
